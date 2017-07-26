9c2a782cd7d782ecb4610f8a43b9b0bb99331238
Onwukike Ibe <oibe@uber.com>
2017-07-26 13:13:16 -0400

    Removed state

--- thrift/meta.thrift
+++ thrift/meta.thrift
@@ -1,17 +1,8 @@
 namespace java com.uber.health
 
-enum State {
-	REFUSING = 0,
-	ACCEPTING = 1,
-	DRAINING = 2,
-	STOPPING = 3,
-	DRAIN_WAITING = 4,
-}
-
 struct HealthStatus {
 	1: required bool ok
 	2: optional string message
-	3: optional State state
 }
 
 service Meta {
